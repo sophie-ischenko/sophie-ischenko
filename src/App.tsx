7d8314e0ea97627494585fa630f297c71fa9cd0a
Sophie <s.ischenko1990@gmail.com>
2026-05-09 19:11:19 +0200

    home.tsx fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,10 @@
-/**
- * @license
- * SPDX-License-Identifier: Apache-2.0
- */
-
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ThemeProvider } from './lib/ThemeContext';
-import { Layout } from './components/Layout';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import './index.css';
+
 export default function App() {
   return (
-    <ThemeProvider>
-      <Router>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Home />} />
-          </Routes>
-        </Layout>
-      </Router>
-    </ThemeProvider>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 }
