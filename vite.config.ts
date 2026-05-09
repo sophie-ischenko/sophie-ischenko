1078486841e48fefd8695c4964c534dfc05815b4
Sophie <s.ischenko1990@gmail.com>
2026-05-09 19:23:53 +0200

    css

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,11 @@
-import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  base: '/sophie-ischenko/',  // Ersetze REPO-NAME mit deinem GitHub Repo-Namen!
-  server: {
-    port: 3000,
-    host: '0.0.0.0',
-  },
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, '.'),
-    }
-  }
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
+  base: '/sophie-ischenko/'
 });
